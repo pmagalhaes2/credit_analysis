d3e28cb68cdf3eb2eb2b61dac92301ac928b7fa9
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-05-26 16:01:58 -0300

    Initial commit

---
+++ src/test/kotlin/br/com/creditas/credit_analysis/CreditAnalysisApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.creditas.credit_analysis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditAnalysisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
