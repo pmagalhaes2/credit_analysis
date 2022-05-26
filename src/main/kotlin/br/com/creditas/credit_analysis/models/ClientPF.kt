b2a492896bc8405077be94eb19b054ddac2ec7ec
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-05-26 16:47:58 -0300

    Feat: create ClientPF.kt entity

---
+++ src/main/kotlin/br/com/creditas/credit_analysis/models/ClientPF.kt
@@ -0,0 +1,28 @@
+package br.com.creditas.credit_analysis.models
+
+import org.hibernate.annotations.DynamicUpdate
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "client_pf")
+@DynamicUpdate
+data class ClientPF(
+    @Id
+    val id: UUID = UUID.randomUUID(),
+
+    @Column(name = "cpf")
+    val cpf: String,
+
+    @Column(name = "name")
+    val name: String,
+
+    @Column(name = "last_name")
+    val lastName: String,
+
+    @Column(name = "birth_date")
+    val birthDate: Date,
+
+    @OneToOne(mappedBy = "client")
+    val address: Address? = null
+)
