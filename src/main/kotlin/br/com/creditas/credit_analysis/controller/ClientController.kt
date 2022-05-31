a36248eb08958ff408443ffb41b33f51f49f8816
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-05-31 10:35:12 -0300

    Fix: remove wildcard imports

--- src/main/kotlin/br/com/creditas/credit_analysis/controller/ClientController.kt
+++ src/main/kotlin/br/com/creditas/credit_analysis/controller/ClientController.kt
@@ -7,9 +7,15 @@ import br.com.creditas.credit_analysis.repositories.ClientRepository
 import br.com.creditas.credit_analysis.requests.AddressDto
 import br.com.creditas.credit_analysis.requests.ClientCreationRequest
 import br.com.creditas.credit_analysis.responses.ClientCreationResponse
-import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.http.ResponseEntity
 import java.util.UUID
 import java.util.Optional
 
