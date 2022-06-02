cdab98f76c405d7af33cb6fe097ca4b589b0b50f
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-06-02 19:59:37 -0300

    Feat: add findByid method

--- src/main/kotlin/br/com/creditas/credit_analysis/repositories/ClientRepository.kt
+++ src/main/kotlin/br/com/creditas/credit_analysis/repositories/ClientRepository.kt
@@ -1,17 +1,15 @@
 package br.com.creditas.credit_analysis.repositories
 
-import br.com.creditas.credit_analysis.exceptions.NotFoundException
 import br.com.creditas.credit_analysis.models.ClientPF
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import org.springframework.web.bind.annotation.PathVariable
 import java.util.UUID
 
 @Repository
 interface ClientRepository: JpaRepository<ClientPF, UUID> {
     fun findByCpf(cpf: String): ClientPF?
 
-    fun findByClientId(id: UUID): ClientPF?
+    fun findByid(id: UUID): ClientPF?
 
     fun deleteByCpf(cpf: String)
 }
