608eb1b5aed503caaf43c7e67da94bd19310bc06
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-06-14 13:32:05 -0300

    Feat: add field validations

--- src/main/kotlin/br/com/creditas/credit_analysis/requests/ClientCreationRequest.kt
+++ src/main/kotlin/br/com/creditas/credit_analysis/requests/ClientCreationRequest.kt
@@ -1,12 +1,31 @@
 package br.com.creditas.credit_analysis.requests
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import org.hibernate.validator.constraints.br.CPF
 import java.util.Date
+import javax.validation.Valid
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Pattern
 
 data class ClientCreationRequest(
+    @field:NotEmpty
+    @field:CPF
     val cpf: String,
+
+    @field:NotEmpty
+    @field:Pattern(regexp = "^[\\pL\\pM\\p{Zs}]+\$")
     val name: String,
+
+    @field:NotEmpty
+    @field:Pattern(regexp = "^[\\pL\\pM\\p{Zs}]+\$")
     val lastName: String,
+
+    @JsonFormat(pattern= ("dd/MM/yyyy"), locale = "pt-BR", timezone = "UTC")
     val birthDate: Date,
+
+    @field:Valid
     val address: AddressDto? = null,
+
+    @field:Valid
     val contacts: List<ContactDto>? = null
 )
--- src/main/kotlin/br/com/creditas/credit_analysis/requests/ClientUpdateRequest.kt
+++ src/main/kotlin/br/com/creditas/credit_analysis/requests/ClientUpdateRequest.kt
@@ -1,12 +1,27 @@
 package br.com.creditas.credit_analysis.requests
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import org.hibernate.validator.constraints.br.CPF
 import java.util.Date
+import javax.validation.Valid
+import javax.validation.constraints.Pattern
 
 data class ClientUpdateRequest(
+    @field:CPF
     val cpf: String,
+
+    @field:Pattern(regexp = "^[\\pL\\pM\\p{Zs}]+\$")
     val name: String,
+
+    @field:Pattern(regexp = "^[\\pL\\pM\\p{Zs}]+\$")
     val lastName: String,
+
+    @JsonFormat(pattern= ("dd/MM/yyyy"), locale = "pt-BR", timezone = "UTC")
     val birthDate: Date,
+
+    @field:Valid
     val address: AddressDto? = null,
-    val contact: ContactDto? = null
+
+    @field:Valid
+    val contacts: List<ContactDto>? = null
 )
